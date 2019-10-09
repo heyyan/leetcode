8fea8f66b05ae012c324acb41d2dbdb243153e48
Heyyan <heyyan@gmail.com>
2019-10-09 21:56:54 +0500

    First
    First

---
+++ Code/Test.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace leetcode.Code
+{
+    public class Test
+    {
+        private IEnumerable<string> collec;
+        public IEnumerable<string> Collection
+
+        {
+            get { return collec; }
+            set { collec = value; }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using leetcode.Code;
+
+namespace leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Test();
+            Console.WriteLine(test?.Collection.Any() ?? false);
+            test.Collection = new List<string>();
+            Console.WriteLine(test?.Collection?.Any() ?? false);
+
+            test.Collection = new List<string>{ "assd","asadasd"};
+
+            Console.WriteLine(test?.Collection?.Any() ?? false);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
+
